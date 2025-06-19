9c7b7c98c8c7c48fdd812cc6aa1eba053799c473
Alexander <jimbox2001@mail.ru>
2025-06-19 21:59:34 +0300

    Minor fix

--- Assets/CodeBase/UI/StatsPanelModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using CodeBase.UI;
-using UnityEngine;
-
-public class StatsPanelModel : MonoBehaviour
-{
-    [SerializeField] private BombsSpawnerView  _bombsSpawnerView;
-}
